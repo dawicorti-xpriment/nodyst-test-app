4553c17e8c5a490acd782fe7124f91e765a4088e
David Corticchiato <dawi@probe.cafe.intra>
2013-11-25 16:31:50 +0100

    init project

---
+++ nodyst-test-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nodyst-test-app
+//
+//  Created by David Corticchiato on 25/11/13.
+//  Copyright (c) 2013 David Corticchiato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nodyst-test-app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nodyst-test-app
+//
+//  Created by David Corticchiato on 25/11/13.
+//  Copyright (c) 2013 David Corticchiato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
