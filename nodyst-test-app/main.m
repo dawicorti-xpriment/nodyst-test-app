f347968fb90ca897f5ce1f1fce5d0070d907aad6
David <david.corticchiato@gmail.com>
2013-11-28 23:01:37 +0100

    hardcoded log with 'log' function

--- nodyst-test-app/main.m
+++ nodyst-test-app/main.m
@@ -10,9 +10,25 @@
 
 #import "AppDelegate.h"
 
+#import "JavaScriptCore/JSContext.h"
+#import "JavaScriptCore/JSExport.h"
+#import "JavaScriptCore/JSValue.h"
+
 int main(int argc, char *argv[])
 {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+
+  JSContext *context = [[JSContext alloc] init];
+
+  context[@"log"] = ^(NSString * msg)
+  {
+      NSLog(msg);
+      return;
+  };
+
+  NSString * jsCode2 =@"log('Hello World !');";
+  [context evaluateScript:jsCode2];
+
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
 }
